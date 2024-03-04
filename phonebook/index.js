d67321de99509891e7c8da6a2624d8edf89d0139
Sean Beaulieu <54913639+seanbeaulieu@users.noreply.github.com>
2024-03-03 19:22:45 -0600

    3.15 - 3.16

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,20 +1,33 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
+app.use(express.static('dist'))
 app.use(express.json())
-
+// pp.use(requestLogger)
 const morgan = require('morgan')
 app.use(morgan('tiny'))
 
 const cors = require('cors')
 app.use(cors())
 
-app.use(express.static('dist'))
+
 
 const Person = require('./models/person')
 
-// const password = process.argv[2]
 
+// Error middleware
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// const password = process.argv[2]
 
 // ADJUST PASSWORD
 
@@ -34,10 +47,15 @@ app.get('/api/persons', (request, response) => {
 
 // GET SPECIFIC RESOURCE    
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
-      response.json(person)
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
   })
 
 // GET INFO 
@@ -56,14 +74,13 @@ app.get('/info', (request, response) => {
 
 // DELETE
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    persons = persons.filter(person => person.id !== id)
-    response.json(person)
-    response.status(204).end()
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.json(result)
+    })
+    .catch(error => next(error))
+})
 
 // POST
 
@@ -95,6 +112,14 @@ const generateId = () => {
     })
   })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+app.use(unknownEndpoint)
+
+// Error middleware
+app.use(errorHandler)
   
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
